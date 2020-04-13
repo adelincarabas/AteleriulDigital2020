77066ebbd0fd0c657d0a15c85dda1403d5f20e5d
adelincarabas <adelinromana@gmail.com>
2020-04-13 21:09:20 +0300

    initial commit

---
+++ src/lab1/Main.java
@@ -0,0 +1,18 @@
+package lab1;
+
+public class Main {
+    private static int semester=2;
+    private int instanceVar=3;
+    public static void main(String[] args) {
+    ControlFlow cf= new ControlFlow();
+    System.out.println("result="+cf.addManyTimes());
+    int nr =6;
+    cf.AutoBox(nr);
+    stringExample();
+    }
+    public static void stringExample(){
+        String s="Java";
+        s=s.concat(" Code");
+        System.out.println(s);
+    }
+}
